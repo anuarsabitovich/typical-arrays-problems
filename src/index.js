4a16e2c4d7f6cd63ac90394da5ce8d70f28d6488
Anuar Sabitovich <anuarkulmagambetov@gmail.com>
2021-03-04 14:54:37 +1100

    fix: return 0

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ exports.min = function min(array) {
 };
 
 exports.max = function max(array) {
-    if (typeof array === undefined || ) {
+    if (typeof array === undefined || array.length === 0) {
         return 0;
     } else return Math.max(...array);
 };
 
 exports.avg = function avg(array) {
-    if (typeof array === undefined ||) {
+    if (typeof array === undefined || array.length === 0) {
         return 0;
     } else {
         let reducer = (accumulator, currentValue) => accumulator + currentValue;
