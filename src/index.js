f101b2835dc6a80e082f9640e3b5579d8764e955
Anuar Sabitovich <anuarkulmagambetov@gmail.com>
2021-03-04 11:07:43 +1100

    feat: add index.html for check

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
+    if (array.length == 0) {
+        return 0;
+    } else return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+   else return Math.max(...array)
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array.length == 0) {
+        return 0;
+    } else {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        const average = array.reduce(reducer) /array.length
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+// function min(array) {
+//     if (array.length == 0) {
+//         console.log("empty");
+//         return 0;
+//     } else console.log(Math.min(...array));
+// }
+// function max(array) {
+//     if (array.length == 0) {
+//         console.log("empty");
+//         return 0;
+//     } else console.log(Math.max(...array));
+// }
+// function avg(array) {
+//     if (array.length == 0) {
+//         console.log("empty");
+//         return 0;
+//     } else {
+//         const reducer = (accumulator, currentValue) =>
+//             accumulator + currentValue;
+//         const sum = array.reduce(reducer);
+//         console.log(sum / array.length);
+//     }
+// }
